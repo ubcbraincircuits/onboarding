29ad9b5b789c5841294ffe42ee49418dd4d130ad
galejo09 <50342003+galejo09@users.noreply.github.com>
2019-08-20 13:44:47 -0700

    Remove matlab extension causing error in hosting

--- source/conf.py
+++ source/conf.py
@@ -46,7 +46,6 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx.ext.autosectionlabel',
-    'sphinxcontrib.matlab',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
